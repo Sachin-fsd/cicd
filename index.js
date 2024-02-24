53198b5185524dfdc5e6f7671f1c2bfd193d3e69
Sachin <sachineducational555@gmail.com>
2024-02-24 13:41:41 +0530

    Fixed: eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const app = require("express")();
 
 app.listen("/",(req,res)=>{
-    res.send("welcome!!!")
-})
+    res.send("welcome!!!");
+});
 
 app.listen(8000,()=>{
     console.log("App live on 8000");
-})
+});
 
 // git init
 // git remote add origin https://github.com/Sachin-fsd/cicd.git
