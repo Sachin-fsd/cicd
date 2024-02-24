5da05a8d75afc2798765d2db8bcd98e5584a5c2d
Sachin <sachineducational555@gmail.com>
2024-02-24 11:31:03 +0530

    Added: initial Project

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+
+app.listen("/",(req,res)=>{
+    res.send("welcome!!!")
+})
+
+app.listen(8000,()=>{
+    console.log("App live on 8000");
+})
+
+// git init
+// git remote add origin https://github.com/Sachin-fsd/cicd.git
+//  git status
